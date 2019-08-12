9c4c399c5c5cc2229c9c53c256c8145054da5fcf
Eduardo Fernandez <yo@edufdezsoy.es>
2019-08-13 00:11:31 +0200

    created status controller

---
+++ controllers/status_controller.js
@@ -0,0 +1,23 @@
+const data = require('../data/data')
+
+exports.getStatus = (req, res) => {
+    data.getStatus(req.query.chapter_id, (err, res2) => {
+        if (err == 400)
+            res.status(400).json('Bad Request, no chapter provided')
+        else if (err)
+            res.status(503).json('Service Unavailable')
+        else
+            res.json(res2)
+    })
+}
+
+exports.setError = (req, res) => {
+    data.setError(req, (err, res2) => {
+        if (err == 400)
+            res.status(400).json('Bad Request')
+        else if (err)
+            res.status(503).json('Service Unavailable')
+        else
+            res.json(res2)
+    })
+}
