2925d1c60d89e22ffce3751ac0d0cdac8e42179e
Eduardo Fernandez <yo@edufdezsoy.es>
2019-08-13 00:10:14 +0200

    created lang controller

---
+++ controllers/lang_controller.js
@@ -0,0 +1,10 @@
+const data = require('../data/data')
+
+exports.getLanguages = (req, res) => {
+    data.getLanguages((err, res2) => {
+        if (err)
+            res.status(503).json('Service Unavailable')
+        else
+            res.json(res2)
+    })
+}
