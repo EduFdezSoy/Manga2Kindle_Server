d46592131f147a30b2e52bb49c4cc34708842731
Eduardo Fernandez <yo@edufdezsoy.es>
2019-08-13 00:10:28 +0200

    error managing

--- controllers/manga_controller.js
+++ controllers/manga_controller.js
@@ -5,20 +5,28 @@ exports.getManga = (req, res) => {
         console.log('GET /manga called (search)')
 
         data.searchManga(req.query.search, (err, res2) => {
-            res.json(res2)
+            if (err)
+                res.status(503).json('Service Unavailable')
+            else
+                res.json(res2)
         })
     } else if (req.query.limit) {
         console.log('GET /manga called (limit)')
 
         data.getMangas(req.query.limit, (err, res2) => {
-            res.json(res2)
+            if (err)
+                res.status(503).json('Service Unavailable')
+            else
+                res.json(res2)
         })
     } else {
         console.log('GET /manga called')
 
         data.getMangas(null, (err, res2) => {
-
-            res.json(res2)
+            if (err)
+                res.status(503).json('Service Unavailable')
+            else
+                res.json(res2)
         })
     }
 }
@@ -28,12 +36,15 @@ exports.putManga = (req, res) => {
         console.log('PUT /manga called')
 
         do {
-            req.query.uuid = 'urn:uuid:74357528-3935-2740-8282-'
+            req.query.uuid = 'urn:uuid:74357528-3935-2740-8282-' // TODO: add this to the .env
             req.query.uuid += Math.floor(Math.random() * (999999999999 - 100000000000) + 100000000000)
         } while (data.uuidExists(req.query.uuid))
-        
+
         data.putManga(req.query, (err, res2) => {
-            res.json(res2)
+            if (err)
+                res.status(503).json('Service Unavailable')
+            else
+                res.json(res2)
         })
     } else {
         console.log('PUT /manga called (Bad Request)')
