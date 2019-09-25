179a2fda98fdb197c72d9795566b182a3f8ea482
Eduardo Fernandez <yo@edufdezsoy.es>
2019-09-25 17:46:21 +0200

    adjusted upload limit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Manga2kindle server v${process.env.VERSION} listening on port ${port}!`))
 
 var jsonParser = bodyParser.json()
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false, limit: '50mb' })
 app.use(jsonParser)
 
 app.use(fileupload({
