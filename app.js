2627f258749d5f33b5e7effe4332cacd5078045c
Eduardo Fernandez <yo@edufdezsoy.es>
2019-09-27 18:58:43 +0200

    added header

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+ * @files Main class, starts the server 
+ * @author Eduardo Fernandez <yo@edufdezsoy.es>
+ * @copyright Eduardo Fernandez 2019
+ * @license CC_BY-NC-SA_4.0
+ */
+
 const express = require('express')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
