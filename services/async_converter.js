e0dc2c3fa9952931c02a1b8196dd7b415ee7aae6
Eduardo Fernandez <yo@edufdezsoy.es>
2019-10-06 00:32:41 +0200

    mangas are not that fast!

--- services/async_converter.js
+++ services/async_converter.js
@@ -9,7 +9,7 @@ class Converter {
                 maxCallsPerWorker: 1,
                 maxConcurrentWorkers: 1,
                 maxRetries: 3,
-                maxCallTime: 45000
+                maxCallTime: 75000
             },
             require.resolve('./converter_for_worker')
         )
