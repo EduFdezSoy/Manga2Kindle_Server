ceda27c40f522905015888de4deac5bb66b55d12
Eduardo Fernandez <yo@edufdez.es>
2020-05-10 15:16:03 +0200

    promised, this module may be removed in another commit

--- api/utils/converter.js
+++ api/utils/converter.js
@@ -5,8 +5,10 @@
  */
 
 // dependencies
-const shell = require('shelljs')
 const path = require('path')
+const rm = require('../utils/rm')
+const kcc = require('../utils/kcc')
+const kindlegen = require('../utils/kindlegen')
 
 /**
  * TODO: write documentation
@@ -16,37 +18,9 @@ const path = require('path')
  * @param {Function} callback Error or null
  */
 exports.FolderToEpub = function (folderName, options, callback) {
-  let style = 'manga' // can be manga, webtoon or comic (others = comic)
-  let splitter = 0 // double page parsing mode. 0: Split 1: Rotate 2: Both
-
-  // escape spaces in the path
-  folderName = folderName.replace(/(\s+)/g, '\\$1')
-
-  if (options) {
-    style = options.style ? options.style : style
-    splitter = Number.isInteger(options.splitter) ? options.splitter : splitter
-  }
-
-  let comand = 'kcc-master/kcc-c2e.py -p KV'
-
-  switch (style) {
-    case 'manga':
-      comand += ' -m'
-      break
-
-    case 'webtoon':
-      comand += ' -w'
-      break
-  }
-
-  comand += ' -2 -u -r ' + splitter + ' -f EPUB ' + folderName
-
-  if (shell.exec(comand).code !== 0) {
-    shell.exit(1)
-    callback(new Error('kcc: conversion to epub failed'))
-  } else {
-    callback(null)
-  }
+  kcc.FolderToEpub(folderName, options)
+    .then((stdout) => callback(null, stdout))
+    .catch((err) => callback(err, null))
 }
 
 /**
@@ -55,15 +29,10 @@ exports.FolderToEpub = function (folderName, options, callback) {
  * @param {Function} callback Error or null
  */
 exports.EpubToMobi = function (file, callback) {
-  // TODO: check if kindlegen is "installed"
   const filePath = path.join(__dirname, '/../output/', file)
-  const comand = 'kindlegen/kindlegen -dont_append_source -locale en "' + filePath + '"'
-  if (shell.exec(comand).code !== 0) {
-    shell.exit(1)
-    callback(new Error('kindlegen conversion failed'))
-  } else {
-    callback(null)
-  }
-  console.log('deleting epub')
-  shell.rm('-rf', filePath)
+
+  kindlegen.EpubToMobi(filePath)
+    .then((stdout) => rm.rmrf(filePath))
+    .then((stdout) => callback(null, stdout))
+    .catch((err) => callback(err, null))
 }
