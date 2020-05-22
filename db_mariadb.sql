d73db51a3a3187cdba14b6c6750a7c8f8da39280
Eduardo Fernandez <yo@edufdez.es>
2020-05-22 22:01:31 +0200

    db files splited

---
+++ db_mariadb.sql
@@ -0,0 +1,100 @@
+/**
+ * This script works for mariadb and mysql
+ * Tested only in mariadb
+ *
+ * @author Eduardo Fernandez
+ * @website https://edufdez.es/
+ */
+
+DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS chapter;
+DROP TABLE IF EXISTS manga;
+DROP TABLE IF EXISTS language;
+DROP TABLE IF EXISTS author;
+
+CREATE TABLE IF NOT EXISTS author (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    nickname VARCHAR(50) NULL
+);
+
+CREATE TABLE IF NOT EXISTS language (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(5),
+    name VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS manga (
+    id SERIAL PRIMARY KEY,
+    uuid VARCHAR(45) UNIQUE,
+    title VARCHAR(150),
+    author_id BIGINT UNSIGNED NOT NULL,
+    CONSTRAINT fk_manga_author
+        FOREIGN KEY (author_id) REFERENCES author(id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS chapter (
+    id SERIAL PRIMARY KEY,
+    manga_id BIGINT UNSIGNED NOT NULL,
+    lang_id BIGINT UNSIGNED NOT NULL,
+    volume INTEGER,
+    chapter FLOAT,
+    title VARCHAR(100),
+    file_path VARCHAR(500),
+    options VARCHAR(50),
+    mail VARCHAR(100),
+    CONSTRAINT fk_chapter_manga
+        FOREIGN KEY (manga_id) REFERENCES manga(id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT fk_chapter_language
+        FOREIGN KEY (lang_id) REFERENCES language(id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS status (
+    id SERIAL PRIMARY KEY,
+    chapter_id BIGINT UNSIGNED NOT NULL,
+    delivered SMALLINT,
+    error SMALLINT,
+    conversion_status SMALLINT NOT NULL DEFAULT 0,
+    reason VARCHAR(250),
+    CONSTRAINT fk_status_chapter
+        FOREIGN KEY (chapter_id) REFERENCES chapter(id) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); 
+
+-- ----------------------------------- TEST DATA
+
+INSERT INTO author (name, surname, nickname) VALUES
+('', '', 'Aldehyde'),
+('Yuuki', 'Obata', ''),
+('Tatsumi', 'Kigi', ''),
+('Aka', 'Akasaka', ''),
+('Yusagi', 'Aneko', ''),
+('', '', 'Katsuwo'),
+('Nagisa', 'Fujita', ''),
+('', '', '774 (Nanashi)'),
+ ('', '', 'Namo'),
+ ('Yamamoto', 'Souichirou', 'Kijouyu Udon');
+
+INSERT INTO language (code, name) VALUES
+('EN', 'English'),
+('ES', 'Español');
+
+INSERT INTO manga (uuid, title, author_id) VALUES
+('urn:uuid:74357528-3935-2740-8282-624925322556', 'Neeko wa Tsurai yo', 1),
+('urn:uuid:74357528-3935-2740-8282-624925305268', 'Bokura ga Ita series', 2),
+('urn:uuid:74357528-3935-2740-8282-624925302374', 'Kyoumei Suru Echo', 3),
+('urn:uuid:74357528-3935-2740-8282-624925308942', 'Kaguya-sama: Love is War', 4),
+('urn:uuid:74357528-3935-2740-8282-624925308451', 'Tate no Yuusha no Nariagari', 5),
+('urn:uuid:74357528-3935-2740-8282-624925308253', 'Hitoribocchi no OO Seikatsu', 6),
+('urn:uuid:74357528-3935-2740-8282-624925308648', 'Do Chokkyuu Kareshi x Kanojo', 7),
+('urn:uuid:74357528-3935-2740-8282-624925308494', 'Ijiranaide, Nagatoro-san', 8),
+('urn:uuid:74357528-3935-2740-8282-624925314569', 'Ookami Shounen wa Kyou mo Uso wo Kasaneru', 9),
+('urn:uuid:74357528-3935-2740-8282-624925313108', 'Karakai Jouzu no Takagi-san', 10);
